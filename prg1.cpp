c44c7ce99725bc63552514bdea10da43a0ee49cd
Devansh0Tyagi-Codes <devansh.25b01011314@abes.ac.in>
2026-08-31 10:57:48 +0530

    prg1 changes

--- prg1.cpp
+++ prg1.cpp
@@ -1,70 +1,55 @@
+// wap to store details of a student name ,roll no., branch,section and marks of 5 subjects .show the details including total marks
+//  and percentage .apply modular programming technique
+
 #include <iostream>
 using namespace std;
+void input(string &name, string &section, string &branch, int &roll, int marks[])
+{
+    cout << "Enter Name : ";
+    cin >> name;
+    cout << "Enter Section : ";
+    cin >> section;
+    cout << "Enter Branch : ";
+    cin >> branch;
+    cout << "Enter roll no : ";
+    cin >> roll;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << " enter marks of " << i + 1 << ":";
+        cin >> marks[i];
+    }
+}
+void calculate(int marks[], int &total, float &percentage)
+{
+    total = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        total += marks[i];
+    }
+    percentage = total / 5.0;
+}
 
+void show(string name, string section, string branch, int roll, int marks[], int total, float percentage)
+{
+    cout << name << endl;
+    cout << section << endl;
+    cout << branch << endl;
+    cout << roll << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Marks of subject " << i + 1 << ": " << marks[i] << endl;
+    }
+    cout << "Total Marks: " << total << endl;
+    cout << "Percentage: " << percentage << endl;
+}
 int main()
 {
-    float balance = 10000, amount;
-    int choice;
-
-  
-        cout << "\n===== ATM MENU =====" << endl;
-        cout << "1. Show Balance" << endl;
-        cout << "2. Deposit Money" << endl;
-        cout << "3. Withdraw Money" << endl;
-        cout << "4. Exit" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            cout << "Current Balance: Rs. " << balance << endl;
-            break;
-
-        case 2:
-            cout << "Enter amount to deposit: ";
-            cin >> amount;
-
-            if (amount > 0)
-            {
-                balance += amount;
-                cout << "Rs. " << amount << " deposited successfully." << endl;
-            }
-            else
-            {
-                cout << "Invalid amount!" << endl;
-            }
-            break;
-
-        case 3:
-            cout << "Enter amount to withdraw: ";
-            cin >> amount;
-
-            if (amount > balance)
-            {
-                cout << "Insufficient Balance!" << endl;
-            }
-            else if (amount <= 0)
-            {
-                cout << "Invalid amount!" << endl;
-            }
-            else
-            {
-                balance -= amount;
-                cout << "Please collect your cash." << endl;
-                cout << "Remaining Balance: Rs. " << balance << endl;
-            }
-            break;
-
-        case 4:
-            cout << "Thank you for using the ATM!" << endl;
-            break;
-
-        default:
-            cout << "Invalid Choice!" << endl;
-        }
-
-    
-
+    string name, section, branch;
+    int roll, marks[5];
+    float percentage;
+    int total;
+    input(name, section, branch, roll, marks);
+    calculate(marks, total, percentage);
+    show(name, section, branch, roll, marks, total, percentage);
     return 0;
 }
