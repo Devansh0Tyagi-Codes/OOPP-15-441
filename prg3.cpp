6f5ecb25a641fb48a6b8b802cb9a2d7c2a0adf22
Devansh0Tyagi-Codes <devansh.25b01011314@abes.ac.in>
2026-08-31 10:51:19 +0530

    changes in prg3

--- prg3.cpp
+++ prg3.cpp
@@ -1,35 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// class complex{
-// private:
-
-//     int real,img;
-// public:
-
-// void input (){
-//     cin >> real >>img;
-// }
-// void show (){
-//     cout << real;
-//     if (img>=0){
-//         cout <<" + "<<img<<"i"<<endl;
-//     }
-//     else{
-//         cout <<img<<" i"<<endl;
-//     }
-// }
-
-// };
-
-// int main (){
-//     complex c1,c2;
-//     c1.input();
-//     c2.input();
-//     c1.show();
-//     c2.show();
-// }
-
 #include <bits/stdc++.h>
 
 using namespace std;
